882fff19c7c0d73289786461b64e8e9721aa6035
sribinai <ammumatambi@gmail.com>
2022-02-18 13:32:56 +0530

    add,sub,mul,div using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDom from 'react-dom';
+
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [addition, setAddition] = useState();
+  const [subtraction, setSubtraction] = useState();
+  const [multiplication, setMultiplication] = useState();
+  const [division, setDivision] = useState();
+ 
+  function add(e){
+    e.preventDefault();
+    // setAddition(number1 + number2);
+    setAddition(parseInt(number1) + parseInt(number2))
+  }
+
+  function sub(e){
+    e.preventDefault();
+    // setSubtraction(parseInt(number1) - parseInt(number2))
+    setSubtraction(number1 - number2)
+  }
+
+  function mul(e){
+    e.preventDefault();
+    setMultiplication(number1 * number2);
+  }
+
+  function div(e){
+    e.preventDefault();
+    setDivision(number1 / number2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+      <form>
+      <div className='inputs'>
+        <label>Number 1 : 
+        <input type="number" 
+        value={number1}
+        onChange={e => setNumber1(e.target.value)}
+        style={{marginLeft: "5px"}} />
+        
+        
+        </label> 
+        <label>Number 2 :
+        <input type="number"
+        value={number2}
+        onChange={e => setNumber2(e.target.value)} 
+        style={{marginLeft: "5px"}} />
+        </label>
+        </div>
+        <div className='buttons'>
+         <button onClick={add} style={{marginRight: "5px"}} ><strong>Add</strong></button>
+         <button onClick={sub} style={{marginRight: "5px"}}><strong>Subtract</strong></button>
+         <button onClick={mul} style={{marginRight: "5px"}}><strong>Multiply</strong></button>
+         <button onClick={div} style={{marginRight: "5px"}}><strong>Divide</strong></button>
+         </div>
+      </form>
+      <h3> {number1} + {number2} = {addition}</h3>
+      <h3> {number1} - {number2} = {subtraction}</h3>
+      <h3> {number1} * {number2} = {multiplication}</h3>
+      <h3> {number1} / {number2} = {division}</h3>
+      </div>
     </div>
+
   );
 }
 
